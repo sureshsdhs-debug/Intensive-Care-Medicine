2a115f43f65dd2f6e2e139914c3d3043013ff681
sureshsarkar <sureshsarkar2020@gmail.com>
2026-02-27 17:33:23 +0530

    searchbar commit

--- frontend/src/admin/includes/Header.jsx
+++ frontend/src/admin/includes/Header.jsx
@@ -22,12 +22,12 @@ const Header = ({ getRole, pageIndex, setPageIndex, setIsManualNavigation }) => 
 
   // Logout handler
   const handleLogout = async () => {
-    try { 
-       let response = await axios.get(
-          `${BACKEND_BASE_URL}/api/student/logout`,
-          { headers: { Authorization: `Bearer ${token}` } }
-        );
-      
+    try {
+      let response = await axios.get(
+        `${BACKEND_BASE_URL}/api/student/logout`,
+        { headers: { Authorization: `Bearer ${token}` } }
+      );
+
       const { data } = response;
 
       if (data?.success) {
@@ -57,59 +57,58 @@ const Header = ({ getRole, pageIndex, setPageIndex, setIsManualNavigation }) => 
         <Link to="/dashboard" onClick={() => goTo(0)}>
           <div className="logo">
             <img className='logo-class' src={logo} alt="Logo" /> </div>
-        </Link> 
+        </Link>
       </div>
- <div className="logosec">
-         <GlobalPageSearch />
- </div>
 
-      {role !=1 && (
-      <div className="logosec">
-        <button className="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
-          <i className="bi bi-list"></i>
-        </button>
+      {role != 1 && (
 
-        <div className="offcanvas offcanvas-end custom-offcanvas"
-          tabIndex="-1"
-          id="offcanvasRight"
-        >
-          <div className="offcanvas-header border-bottom">
-            <h5 className="fw-bold mb-0 text-color">📖 Page Menus</h5>
-            <button
-              type="button"
-              className="btn-close"
-              data-bs-dismiss="offcanvas"
-            ></button>
-          </div>
+        <div className="logosec">
+           <GlobalPageSearch />
+          <button className="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
+            <i className="bi bi-list"></i>
+          </button>
 
-          <div className="offcanvas-body p-0">
-            {[
-              "Copyright",
-              "Table of Contents",
-              "Acknowledgement",
-              "Dr. Sanjay",
-              "Dr. Aashish",
-              "Dr Prakash",
-              "About Author",
-              "Preface",
-            ].map((item, index) => (
-              <div
-                key={index}
-                onClick={() => goTo(index + 1)}
-                className={`menu-item ${pageIndex === index + 1 ? "active-menu" : ""
-                  }`}
+          <div className="offcanvas offcanvas-end custom-offcanvas"
+            tabIndex="-1"
+            id="offcanvasRight"
+          >
+            <div className="offcanvas-header border-bottom">
+              <h5 className="fw-bold mb-0 text-color">📖 Page Menus</h5>
+              <button
+                type="button"
+                className="btn-close"
                 data-bs-dismiss="offcanvas"
-              >
-                {item}
-              </div>
-            ))}
+              ></button>
+            </div>
 
-            <p onClick={handleLogout} className="menu-item logout-btn">
-              <i className="bi bi-box-arrow-right"></i> Logout
-            </p>
+            <div className="offcanvas-body p-0">
+              {[
+                "Copyright",
+                "Table of Contents",
+                "Acknowledgement",
+                "Dr. Sanjay",
+                "Dr. Aashish",
+                "Dr Prakash",
+                "About Author",
+                "Preface",
+              ].map((item, index) => (
+                <div
+                  key={index}
+                  onClick={() => goTo(index + 1)}
+                  className={`menu-item ${pageIndex === index + 1 ? "active-menu" : ""
+                    }`}
+                  data-bs-dismiss="offcanvas"
+                >
+                  {item}
+                </div>
+              ))}
+
+              <p onClick={handleLogout} className="menu-item logout-btn">
+                <i className="bi bi-box-arrow-right"></i> Logout
+              </p>
+            </div>
           </div>
-        </div> 
-      </div>
+        </div>
       )}
     </header>
   );
